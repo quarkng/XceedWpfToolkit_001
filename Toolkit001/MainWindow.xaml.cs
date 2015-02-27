505d33aeae6dd978865e363f4fe9777c3c1ea914
Quark Ng <qn.git@outlook.com>
2015-02-27 09:42:07 -0600

    Initial code commit.

---
+++ Toolkit001/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+using Xceed.Wpf.DataGrid; 
+
+namespace Toolkit001
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void ButtonSpinner_Spin(object sender, Xceed.Wpf.Toolkit.SpinEventArgs e)
+        {
+            ButtonSpinner spinner = (ButtonSpinner)sender;
+            TextBox txtBox = (TextBox)spinner.Content;
+
+            int value = String.IsNullOrEmpty(txtBox.Text) ? 0 : Convert.ToInt32(txtBox.Text);
+            if (e.Direction == Xceed.Wpf.Toolkit.SpinDirection.Increase)
+                value++;
+            else
+                value--;
+            txtBox.Text = value.ToString();
+        }
+    }
+}
